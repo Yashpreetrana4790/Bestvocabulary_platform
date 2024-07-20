9e69bd4e931a27a60b6f4e1cb6c126555006ae0a
Yashpreetrana4790 <109335257+Yashpreetrana4790@users.noreply.github.com>
2024-07-20 20:25:20 +0530

    dashboard err

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -1,11 +1,4 @@
 
-import React from 'react';
-import 'primeflex/primeflex.css'; // Optional: PrimeFlex for grid system
-import 'primereact/resources/themes/saga-blue/theme.css'; // PrimeReact theme
-import 'primereact/resources/primereact.min.css'; // PrimeReact components
-import 'primeicons/primeicons.css';
-import 'primeicons/primeicons.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 export const metadata = {
