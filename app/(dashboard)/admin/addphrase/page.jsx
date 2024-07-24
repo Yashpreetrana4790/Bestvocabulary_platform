bc0cc7a41f762ea0d60571b6007ded580bc604c2
Yashpreetrana4790 <109335257+Yashpreetrana4790@users.noreply.github.com>
2024-07-24 22:18:29 +0530

    fixed the issue  of deployment

--- app/(dashboard)/admin/addphrase/page.jsx
+++ app/(dashboard)/admin/addphrase/page.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import IdiomsPopup from '../../components/AddIdiomForm'
 
 const page = () => {
   return (
     <div>
-      <IdiomsPopup />
+      {/* <IdiomsPopup /> */}
     </div>
   )
 }
