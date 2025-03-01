1deac095eda68e00b2655eab81dfbb33cb99bebb
Yashpreet Rana <codecrunchenthusiast@gmail.com>
2025-03-01 11:31:06 +0530

    format fix and lint and other issue

--- app/(root)/(home)/(sidebarroutes)/wordofday/page.jsx
+++ app/(root)/(home)/(sidebarroutes)/wordofday/page.jsx
@@ -6,32 +6,36 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { capitalizeString } from "@/lib/helper"
 import { HighlightedHead } from "@/components/HighlightedHead"
 import { useEffect, useState } from "react"
-const page = () => {
-  const [oneWord, setoneWord] = useState(null)
 
+const Page = () => {
+  const [oneWord, setOneWord] = useState(null)
 
   const max = 5000
+
   useEffect(() => {
     const num = getRandomInt(max)
-    const oneWord = wordlist[num]
-    setoneWord(oneWord)
+    const word = wordlist[num]
+    setOneWord(word)
   }, [])
 
   function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * max)
   }
+
   return (
     <div>
       <PageHeader>
         <HighlightedHead />
-        <PageHeaderHeading >
+        <PageHeaderHeading>
           {capitalizeString(oneWord?.word || "Loading...")}
         </PageHeaderHeading>
         <PageHeaderDescription>
-          <p className="text-lg font-medium mb-2"> {oneWord?.response?.pronunciation}</p>
+          <p className="text-lg font-medium mb-2">{oneWord?.response?.pronunciation}</p>
         </PageHeaderDescription>
         <div>
-          <span className="p-2 rounded-2xl bg-secondary my-4 mx-10 ">{oneWord?.response?.meanings?.[0]?.subtitle || "Loading..."}</span>
+          <span className="p-2 rounded-2xl bg-secondary my-4 mx-10">
+            {oneWord?.response?.meanings?.[0]?.subtitle || "Loading..."}
+          </span>
         </div>
         <div className="container">
           {oneWord?.response && (
@@ -42,16 +46,16 @@ const page = () => {
         </div>
         <Card className="my-4 rounded-none lg:min-w-7xl mx-2">
           <CardHeader>
-            <CardTitle className=" text-xl text-black dark:text-white text-center font-bold">Common Usage</CardTitle>
+            <CardTitle className="text-xl text-black dark:text-white text-center font-bold">
+              Common Usage
+            </CardTitle>
           </CardHeader>
           <CardContent>
             {oneWord?.response?.meanings?.map((meaning, index) => (
-              <div key={index} className="mb-2 ">
+              <div key={index} className="mb-2">
                 {meaning?.common_usage?.map((usage, idx) => (
                   <div key={idx}>
-                    <CardTitle className="text-md text-black dark:text-white" >
-                      {usage?.context}
-                    </CardTitle>
+                    <CardTitle className="text-md text-black dark:text-white">{usage?.context}</CardTitle>
                     <CardDescription>
                       <p className="text-base text-muted-foreground mb-2">Example: {usage?.example}</p>
                     </CardDescription>
@@ -62,9 +66,8 @@ const page = () => {
           </CardContent>
         </Card>
       </PageHeader>
-
     </div>
   )
 }
 
-export default page
+export default Page
