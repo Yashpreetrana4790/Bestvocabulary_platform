2a764568483607dced6ddb235c1dc8f1151ea0fc
Yashpreetrana4790 <codecrunchenthusiast@gmail.com>
2025-11-28 11:02:32 +0530

    final changes:

--- workbook.js
+++ workbook.js
@@ -9,3 +9,7 @@
 // Todo //
 // 1. Create the admin dashboard ( post the word form cards showing the total no of words on site as per the categories  )
 // 2. Word Cards will be having  the 3 categories of dots showing the level of word  and also a case when it is having some advance scenarios
+// 3. if I search walk word then there must be dropdown showing in the search bar for relating words and also a case when it is having some advance scenarios
+//4  Now suppose there is one word skill then that word can be related to multiple  sub cases kike skill of doing something and skill of using body in better way so these are relation  so want to make thm go in one category
+
+// need a chat bot for my website
